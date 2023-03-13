c9c9f00c054bfd46051c2c43f854dfb696768f9e
Dave Bratby <dbratby1992@gmail.com>
2023-03-13 20:27:26 +0000

    Added a age property to the Pet function. Created the Prototype funtion to incremenet the age by 1 and also added the test to check the inital age of the pet is 0

---
+++ __test__/pet.test.js
@@ -0,0 +1,16 @@
+const Pet = require ('../src/pet.js')
+
+describe('constructor', () => {
+    it('returns an object', () => {
+        expect(new Pet ('Fido')).toBeInstanceOf(Object);
+    });
+    it('sets the name property', () => {
+        const pet = new Pet('Fido');
+        expect(pet.name).toEqual('Fido');
+      });
+    it('has an initial age of 0', () => {
+      const pet = new Pet('Fido')
+      pet.growUp()
+      expect(pet.age).toEqual(1);
+    })
+    });
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,10 @@
-function pet () {
+function Pet (name) {
+    this.name=name;
+    this.age=0;
+    };
 
-}
+    Pet.prototype.growUp = function () {
+        this.age += 1
+    };
 
-module.exports = pet;
+module.exports = Pet;
