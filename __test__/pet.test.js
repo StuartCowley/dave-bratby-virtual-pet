d3394618e485d9841a4e18bb9ff382af78afa617
Dave Bratby <dbratby1992@gmail.com>
2023-03-14 13:46:07 +0000

    added the become unfit and the walk prototypes to the Pet. refactored to remove some magic mumbers from the code

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -13,4 +13,29 @@ describe('constructor', () => {
       pet.growUp()
       expect(pet.age).toEqual(1);
     })
+    it('has inital hunger of 0', () => {
+      const pet = new Pet ('Fido')
+      pet.getHungry()
+      expect(pet.hunger).toEqual(5)
+    })
+    it('has inital fitness of 10', () => {
+      const pet = new Pet ('Fido')
+      pet.becomeUnfit()
+      expect(pet.fitness).toEqual(7)
+    })
     });
+
+    describe('walk', () => {
+      it('increases fitness by 4', () => {
+        const pet = new Pet('Fido')
+        pet.fitness = 4;
+        pet.walk()
+        expect(pet.fitness).toEqual(8)
+      })
+      it('it increases fitness to a maximum of 10', () => {
+        const pet = new Pet ('Fido')
+        pet.fitness = 8;
+        pet.walk()
+        expect(pet.fitness).toEqual(10)
+      })
+    })
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,30 @@
+const max_fitness = 10;
+const empty_belly = 0;
+const grow_old = 1;
+
 function Pet (name) {
-    this.name=name;
-    this.age=0;
+    this.name = name;
+    this.age = 0;
+    this.hunger = empty_belly;
+    this.fitness = max_fitness;
     };
 
-    Pet.prototype.growUp = function () {
-        this.age += 1
+    Pet.prototype.growUp = function (){
+        this.age += grow_old;
     };
+    Pet.prototype.getHungry = function (){ 
+        this.hunger += 5;
+    };
+    Pet.prototype.becomeUnfit = function (){
+        this.fitness -= 3;
+    };
+    Pet.prototype.walk =  function (){
+        if((this.fitness + 4) <= max_fitness){
+            this.fitness += 4
+        } else {
+            this.fitness = max_fitness
+        }
+    };
+    
 
-module.exports = Pet;
+module.exports = Pet
